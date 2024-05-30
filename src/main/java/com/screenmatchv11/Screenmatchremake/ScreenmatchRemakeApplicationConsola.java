dd4e1b45ddb6b490b0bb7c33c31d02c2ffc6f2a5
Cristian Sigel <136412255+zotel1@users.noreply.github.com>
2024-05-30 01:44:54 -0300

    Add files via upload
    transformando nuestra aplicacion en una api consumible por el front end

---
+++ src/main/java/com/screenmatchv11/Screenmatchremake/ScreenmatchRemakeApplicationConsola.java
@@ -0,0 +1,24 @@
+//package com.screenmatchv11.Screenmatchremake;
+//import com.screenmatchv11.Screenmatchremake.principal.Principal;
+//import com.screenmatchv11.Screenmatchremake.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class ScreenmatchRemakeApplicationConsola implements CommandLineRunner {
+
+//	@Autowired
+//	private SerieRepository repository;
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchRemakeApplicationConsola.class, args);
+//	}
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Principal principal = new Principal(repository);
+//		principal.muestraElMenu();
+//	}
+//}
---
+++ src/main/java/com/screenmatchv11/Screenmatchremake/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.screenmatchv11.Screenmatchremake.model;
+
+public enum Categoria {
+    ACCION("Action", "Acción"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comedia"),
+    CRIMEN("Crime", "Trama"),
+    DRAMA("Drama", "Crimen"),
+    BIOGRAFIA("Biography", "Biografía");
+
+    private String categoriaOmdb;
+
+    private String categoriaEspanol;
+
+    Categoria (String categoriaOmdb, String categoriaEspanol) {
+
+        this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEspanol = categoriaEspanol;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada." + text);
+    }
+
+    public static Categoria fromEspanol(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaEspanol.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada." + text);
+    }
+}
